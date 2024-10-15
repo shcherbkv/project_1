dcac989611f9c2bf33d5c3afab43d13b249bd4c1
shcherbkv <serbakovmaks734@gmail.com>
2024-10-16 00:36:22 +0300

    Logic for 2 operands

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,43 @@ $(document).ready(function(){
 
 /* Добавление цифры в keyboard ------------------------------------------------------------------- */
 
+		let operand1;
+		let operand2;
+		let operator1;
+		let result;
+
 	$(".keyboard").on("click", function(event) {
-		if (event.target.value !== "=") {
+
+/* Если нажали на цифру или оператор, то выводим их значение на экран ---------------------------- */
+		if ( $(event.target).hasClass("figure") || $(event.target).hasClass("operator") ) {
 			$(".calc-number").append(event.target.value);
 		};
+/* Если нажали на оператор, то сохраняем значение оператора и первого операнда ------------------- */
 		if ( $(event.target).hasClass("operator") ) {
-			const operator1 = event.target.value;
-			const operand1 = $(".calc-number").text().slice(0, -1);
+			operator1 = event.target.value;
+			operand1 = $(".calc-number").text().slice(0, -1);
 			console.log(operand1);
-		};
-		if ()
-	})
+		}; 
+/* Если нажали на равно, то ---------------------------------------------------------------------- */
+		if (event.target.value === "=") {
+/* Сохраняем значение вторового операнда --------------------------------------------------------- */
+			const unnecessary = operand1.length + 1;
+			operand2 = $(".calc-number").text().slice(unnecessary);
+			console.log(operand2);
+/* */
+			if (operator1 === "+") {
+				result = Number(operand1) + Number(operand2);
+			} else if (operator1 === "-") {
+				result = Number(operand1) - Number(operand2);
+			} else if (operator1 === "*") {
+				result = Number(operand1) * Number(operand2);
+			} else if (operator1 === "/") {
+				result = Number(operand1) / Number(operand2);
+			}
+/* */
+			$(".itog-number").text(result);
+		}
+	});
 
 });
 
